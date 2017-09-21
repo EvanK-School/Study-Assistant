0123301b33c83921c50b79e980098a65748cc54f
Evan Keeton <2044295@edison.k12.nj.us>
2017-09-21 17:53:26 -0400

    Add to `choice()` ability to execute other python commands.
    Most recent features relatively untested. More testing necessary.

--- definitions.py
+++ definitions.py
@@ -30,24 +30,29 @@ def choice(mydic, string=None, num=4):
     if not(rslt in choices):
         index = rd.randint(0, 3)
         choices[index] = rslt
+    while True:
+        print('Define: {0}'.format(string))
+        print()
+        for x, y in enumerate(choices):    print(alpha[x] + ' ' + str(y))
+        print()
 
-    print('Define: {0}'.format(string))
-    print()
-    for x, y in enumerate(choices):    print(alpha[x] + ' ' + str(y))
-    print()
-    answer = input('\007>>> ')
-    if not(answer == rslt):
-        if len(answer) > 2: answer = answer[:2]
-        if len(answer) < 2: answer += '.'
-        if not(answer[1] == '.'): answer = answer[0] + '.'
-    print()
-    if answer == rslt:
-        print(answer == rslt)
-    elif choices[alpha.index(answer)] == rslt:
-        print(choices[alpha.index(answer)] == rslt)
-    else:
-        print(False, rslt, sep='\n')
-    print()
+        answer = input('\007>>> ')
+        try:
+            eval(answer)
+        except:
+            if not(answer == rslt):
+                if len(answer) > 2: answer = answer[:2]
+                if len(answer) < 2: answer += '.'
+                if not(answer[1] == '.'): answer = answer[0] + '.'
+            print()
+            if answer == rslt:
+                print(answer == rslt)
+            elif choices[alpha.index(answer)] == rslt:
+                print(choices[alpha.index(answer)] == rslt)
+            else:
+                print(False, rslt, sep='\n')
+            print()
+            break
 
 # Note that if only definitions.py is imported,
 # will default to import definitions
